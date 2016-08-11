59127efe10aef48a7e76ff613a79220e098c4b0f
Maan Anayee <maanjoseph@gmail.com>
2016-08-11 15:49:15 +0000

    test

--- py/script.py
+++ py/script.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.BCM)
 
 # init list with pin numbers
 
-pinList = [2, 3, 4, 17]
+pinList = [2, 3]
 
 # loop through pins and set mode and state to 'high'
 
@@ -27,14 +27,7 @@ try:
   GPIO.output(3, GPIO.LOW)
   print "TWO"
   time.sleep(SleepTimeL);  
-  GPIO.output(4, GPIO.LOW)
-  print "THREE"
-  time.sleep(SleepTimeL);
-  GPIO.output(17, GPIO.LOW)
-  print "FOUR"
-  time.sleep(SleepTimeL);
   GPIO.cleanup()
-  print "Good bye!"
 
 # End program cleanly with keyboard
 except KeyboardInterrupt:
